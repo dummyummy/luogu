bfd23a7489d3685cc1f91b9eba4a094ffea47da4
dummyummy <3384447470@qq.com>
2019-04-23 09:13:39 +0800

    greater complexity for p4725 and p4726

--- P4725 多项式对数函数/main.cpp
+++ P4725 多项式对数函数/main.cpp
@@ -1,18 +1,19 @@
-#include <bits/stdc++.h>
+#include <cstdio>
+#include <cstring>
+#include <algorithm>
 
 using namespace std;
 
-#define ll long long
 #define N 100000
 #define MOD 998244353
 
 int n;
-int f0[4*N+5], f[4*N+5], g[4*N+5], h[4*N+5], f1[4*N+5];
+int f[4*N+5], g[4*N+5], h[4*N+5], f1[4*N+5], g1[4*N+5], h1[4*N+5];
 
 int fpow(int x, int p) {
   int ret = 1;
   while(p) {
-    if(p&1) ret = 1LL*x*ret%MOD;
+    if(p&1) ret = 1LL*ret*x%MOD;
     x = 1LL*x*x%MOD;
     p >>= 1;
   }
@@ -20,79 +21,70 @@ int fpow(int x, int p) {
 }
 
 void bitReverse(int *s, int len, int bit) {
-  for(int i = 0; i < len; ++i) {
-    int t = 0;
-    for(int j = 0; j < bit; ++j) if((i>>j)&1) t |= 1<<(bit-1-j);
-    if(i < t) swap(s[i], s[t]);
+  static int rev[4*N+5];
+  for(int i = 1; i < len; ++i) {
+    rev[i] = (rev[i>>1]>>1)|((i&1)<<(bit-1));
+    if(i < rev[i]) swap(s[i], s[rev[i]]);
   }
 }
 
-void DFT(int *s, int flag, int len, int bit) {
+void NTT(int *s, int len, int bit, int flag) {
   bitReverse(s, len, bit);
   for(int l = 2; l <= len; l <<= 1) {
     int mid = l>>1, t = fpow(3, (MOD-1)/l);
-    if(flag) t = fpow(t, MOD-2);
+    if(flag == -1) t = fpow(t, MOD-2);
     for(int *p = s; p != s+len; p += l) {
       int w = 1;
       for(int i = 0; i < mid; ++i) {
-        int x = p[i], y = 1LL*w*p[i+mid]%MOD;
-        p[i] = (x+y)%MOD, p[i+mid] = (x-y)%MOD;
+        int x = 1LL*w*p[i+mid]%MOD;
+        p[i+mid] = (p[i]-x)%MOD, p[i] = (p[i]+x)%MOD;
         w = 1LL*w*t%MOD;
       }
     }
   }
-}
-
-void inv(int *f, int *g, int c) {
-  if(c == 1) {
-    g[0] = fpow(f[0], MOD-2);
-    return ;
+  if(flag == -1) {
+    int inv = fpow(len, MOD-2);
+    for(int i = 0; i < len; ++i) s[i] = 1LL*s[i]*inv%MOD;
   }
-  inv(f, g, (c+1)/2);
-  int bit = 0, len, invlen, lim = c+(c+1)/2-1;
-  while((1<<bit) < lim) bit++;
-  len = (1<<bit), invlen = fpow(len, MOD-2);
-  memset(f0, 0, sizeof(int)*len), memcpy(f0, f, sizeof(int)*c);
-  DFT(f0, 0, len, bit), DFT(g, 0, len, bit);
-  for(int i = 0; i < len; ++i) f0[i] = 1LL*f0[i]*g[i]%MOD;
-  DFT(f0, 1, len, bit);
-  f0[0] = 2-1LL*f0[0]*invlen%MOD;
-  for(int i = 1; i < lim; ++i) f0[i] = i < c ? -1LL*f0[i]*invlen%MOD : 0;
-  DFT(f0, 0, len, bit);
-  for(int i = 0; i < len; ++i) g[i] = 1LL*g[i]*f0[i]%MOD;
-  DFT(g, 1, len, bit);
-  for(int i = 0; i < lim; ++i) g[i] = i < c ? 1LL*g[i]*invlen%MOD : 0;
 }
 
-void d(int *f, int *g, int c) {
-  g[c-1] = 0;
-  for(int i = 1; i < c; ++i) g[i-1] = 1LL*i*f[i]%MOD;
+void polyInv(int *f, int *g, int bit) {
+  if(bit == 0) { g[0] = fpow(f[0], MOD-2); return ; }
+  polyInv(f, g, bit-1);
+  int len = 1<<bit, lim = len<<1;
+  for(int i = 0; i < len; ++i) f1[i] = f[i];
+  NTT(f1, lim, bit+1, 1), NTT(g, lim, bit+1, 1);
+  for(int i = 0; i < lim; ++i) g[i] = 1LL*g[i]*(2-1LL*f1[i]*g[i]%MOD)%MOD;
+  NTT(g, lim, bit+1, -1);
+  for(int i = len; i < lim; ++i) g[i] = 0;
 }
 
-void _d(int *f, int *g, int c) {
+void d(int *f, int *g, int len) {
+  g[len-1] = 0;
+  for(int i = 1; i < len-1; ++i) g[i-1] = 1LL*i*f[i]%MOD;
+}
+
+void d_(int *f, int *g, int len) {
   g[0] = 0;
-  for(int i = 1; i <= c; ++i) g[i] = 1LL*f[i-1]*fpow(i, MOD-2)%MOD;
+  for(int i = 1; i < len; ++i) g[i] = 1LL*fpow(i, MOD-2)*f[i-1]%MOD;
 }
 
-void ln(int *f, int *g, int c) {
-  int bit = 0, len, invlen;
-  while((1<<bit) < 2*c-1) bit++;
-  len = (1<<bit), invlen = fpow(len, MOD-2);
-  inv(f, h, c);
-  d(f, f1, c);
-  DFT(h, 0, len, bit), DFT(f1, 0, len, bit);
-  for(int i = 0; i < len; ++i) h[i] = 1LL*h[i]*f1[i]%MOD;
-  DFT(h, 1, len, bit);
-  for(int i = 0; i < len; ++i) h[i] = i < c ? 1LL*h[i]*invlen%MOD : 0;
-  _d(h, g, c);
-  g[c] = 0;
+void polyLn(int *f, int *g, int bit) {
+  int len = 1<<bit, lim = len<<1;
+  polyInv(f, g, bit); d(f, g1, len);
+  NTT(g, lim, bit+1, 1), NTT(g1, lim, bit+1, 1);
+  for(int i = 0; i < lim; ++i) g1[i] = 1LL*g[i]*g1[i]%MOD;
+  NTT(g1, lim, bit+1, -1);
+  d_(g1, g, len);
 }
 
 int main() {
   scanf("%d", &n);
-  for(int i = 0; i <= n-1; ++i) scanf("%d", &f[i]);
-  ln(f, g, n);
-  for(int i = 0; i <= n-1; ++i) printf("%d ", (g[i]+MOD)%MOD);
+  for(int i = 0; i < n; ++i) scanf("%d", &f[i]);
+  int bit = 0;
+  while((1<<bit) <= n) bit++;
+  polyLn(f, g, bit);
+  for(int i = 0; i < n; ++i) printf("%d ", (g[i]+MOD)%MOD);
   puts("");
   return 0;
 }
