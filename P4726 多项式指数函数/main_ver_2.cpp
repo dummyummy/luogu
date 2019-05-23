286ced42923c09b178896fce6cfc986811fdd2ef
dummyummy <3384447470@qq.com>
2019-05-23 23:43:21 +0800

    add a new version for p4726 with polySqrt

---
+++ P4726 多项式指数函数/main_ver_2.cpp
@@ -0,0 +1,120 @@
+#include <cstdio>
+#include <cstring>
+#include <algorithm>
+
+using namespace std;
+
+#define N 100000
+#define MOD 998244353
+
+int n;
+int f[4*N+5], g[4*N+5], f1[4*N+5], s1[4*N+5], s2[4*N+5], g1[4*N+5], h1[4*N+5];
+
+int fpow(int x, int p) {
+  int ret = 1;
+  while(p) {
+    if(p&1) ret = 1LL*ret*x%MOD;
+    x = 1LL*x*x%MOD;
+    p >>= 1;
+  }
+  return ret;
+}
+
+void bitReverse(int *s, int len, int bit) {
+  static int rev[4*N+5];
+  for(int i = 1; i < len; ++i) {
+    rev[i] = (rev[i>>1]>>1)|((i&1)<<(bit-1));
+    if(i < rev[i]) swap(s[i], s[rev[i]]);
+  }
+}
+
+void NTT(int *s, int len, int bit, int flag) {
+  bitReverse(s, len, bit);
+  for(int l = 2; l <= len; l <<= 1) {
+    int mid = l>>1, t = fpow(3, (MOD-1)/l);
+    if(flag == -1) t = fpow(t, MOD-2);
+    for(int *p = s; p != s+len; p += l) {
+      int w = 1;
+      for(int i = 0; i < mid; ++i) {
+        int x = 1LL*w*p[i+mid]%MOD;
+        p[i+mid] = (p[i]-x)%MOD, p[i] = (p[i]+x)%MOD;
+        w = 1LL*w*t%MOD;
+      }
+    }
+  }
+  if(flag == -1) {
+    int inv = fpow(len, MOD-2);
+    for(int i = 0; i < len; ++i) s[i] = 1LL*s[i]*inv%MOD;
+  }
+}
+
+void polyInv(int *f, int *g, int bit) { //f1
+  if(bit == 0) { g[0] = fpow(f[0], MOD-2); return ; }
+  polyInv(f, g, bit-1);
+  int len = 1<<bit, lim = len<<1;
+  for(int i = 0; i < len; ++i) f1[i] = f[i];
+  NTT(f1, lim, bit+1, 1), NTT(g, lim, bit+1, 1);
+  for(int i = 0; i < lim; ++i) g[i] = 1LL*g[i]*(2-1LL*f1[i]*g[i]%MOD)%MOD;
+  NTT(g, lim, bit+1, -1);
+  for(int i = len; i < lim; ++i) g[i] = 0;
+}
+
+void polySqrt(int *f, int *g, int bit) { //s1 and s2
+    if(bit == 0) {
+        g[0] = 1;
+        return ;
+    }
+    polySqrt(f, g, bit-1);
+    int len = 1<<bit, lim = len<<1, inv2 = fpow(2, MOD-2);
+    for(int i = 0; i < len; ++i) f1[i] = s1[i] = 0, s2[i] = f[i];
+    polyInv(g, s1, bit);
+    NTT(s2, lim, bit+1, 1), NTT(s1, lim, bit+1, 1);
+    for(int i = 0; i < lim; ++i) s1[i] = 1LL*s1[i]*s2[i]%MOD;
+    NTT(s1, lim, bit+1, -1);
+    for(int i = 0; i < len; ++i) g[i] = 1LL*(s1[i]+g[i])%MOD*inv2%MOD;
+}
+
+void d(int *f, int *g, int len) {
+  g[len-1] = 0;
+  for(int i = 1; i < len-1; ++i) g[i-1] = 1LL*i*f[i]%MOD;
+}
+
+void d_(int *f, int *g, int len) {
+  g[0] = 0;
+  for(int i = 1; i < len; ++i) g[i] = 1LL*fpow(i, MOD-2)*f[i-1]%MOD;
+}
+
+void polyLn(int *f, int *g, int bit) { //g1
+  int len = 1<<bit, lim = len<<1;
+  polyInv(f, g, bit); d(f, g1, len);
+  NTT(g, lim, bit+1, 1), NTT(g1, lim, bit+1, 1);
+  for(int i = 0; i < lim; ++i) g1[i] = 1LL*g[i]*g1[i]%MOD;
+  NTT(g1, lim, bit+1, -1);
+  d_(g1, g, len);
+  for(int i = len; i < lim; ++i) g[i] = 0;
+}
+
+void polyExp(int *f, int *g, int bit) { //h1
+  if(bit == 0) { g[0] = 1; return ; }
+  polyExp(f, g, bit-1);
+  int len = 1<<bit, lim = len<<1;
+  for(int i = 0; i < lim; ++i) g1[i] = f1[i] = h1[i] = 0;
+  polyLn(g, h1, bit);
+  h1[0] = (1-h1[0]+f[0])%MOD;
+  for(int i = 1; i < len; ++i) h1[i] = (-h1[i]+f[i])%MOD;
+  NTT(g, lim, bit+1, 1), NTT(h1, lim, bit+1, 1);
+  for(int i = 0; i < lim; ++i) g[i] = 1LL*g[i]*h1[i]%MOD;
+  NTT(g, lim, bit+1, -1);
+  for(int i = len; i < lim; ++i) g[i] = 0;
+}
+
+int main() {
+  scanf("%d", &n);
+  for(int i = 0; i < n; ++i) scanf("%d", &f[i]);
+  int bit = 0;
+  while((1<<bit) <= n) bit++;
+  polyExp(f, g, bit);
+  for(int i = 0; i < n; ++i) printf("%d ", (g[i]+MOD)%MOD);
+  puts("");
+  return 0;
+}
